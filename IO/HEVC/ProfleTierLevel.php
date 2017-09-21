edaa6e079d39fd928f4e7066f95e1ecf3ba0b1eb
Yoshihiro Yamazaki <yoya@awm.jp>
2017-09-21 18:09:54 +0900

    dump method.

--- IO/HEVC/ProfleTierLevel.php
+++ IO/HEVC/ProfleTierLevel.php
@@ -4,10 +4,16 @@ require_once 'IO/Bit.php';
 
 class IO_HEVC_ProfileTierLevel {
     function parse($bit, $profilePresentFlag, $maxnumSubLayersMinus1) {
+        $this->profilePresentFlag = $profilePresentFlag;
+        $this->maxnumSubLayersMinus1 = $maxnumSubLayersMinus1;
         $profile = [];
         if ($profilePresentFlag) {
+            
         }
         $maxnumSubLayersMinus1;
         return [];
+    }
+    function dump() {
+        ;
     }
 }
