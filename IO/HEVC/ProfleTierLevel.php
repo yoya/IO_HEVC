81cc0767244a8e7567da20f6dcf2f67a1a6c6c0c
Yoshihiro Yamazaki <yoya@awm.jp>
2017-09-28 15:05:15 +0900

    profile_tier_level parse to general_level_idc

--- IO/HEVC/ProfleTierLevel.php
+++ IO/HEVC/ProfleTierLevel.php
@@ -5,7 +5,7 @@ class IO_HEVC_ProfileTierLevel {
     function __construct() {
         $this->dump = new IO_HEVC_Dump();
     }
-    function parse($bit, $profilePresentFlag, $maxnumSubLayersMinus1) {
+    function parse($bit, $profilePresentFlag, $maxumSubLayersMinus1) {
         $this->profilePresentFlag = $profilePresentFlag;
         $this->maxnumSubLayersMinus1 = $maxnumSubLayersMinus1;
         $profile = [];
@@ -23,7 +23,7 @@ class IO_HEVC_ProfileTierLevel {
             $this->general_interlaced_source_flag = $bit->getUIBit();
             $this->general_non_packed_constraint_flag = $bit->getUIBit();
             $this->general_frame_only_constraint_flag = $bit->getUIBit();
-            if (($general_profile_idc === 4) || ($general_profile_compatibility_flag[4]) ||
+2            if (($general_profile_idc === 4) || ($general_profile_compatibility_flag[4]) ||
                 ($general_profile_idc === 5) || ($general_profile_compatibility_flag[5]) ||
                 ($general_profile_idc === 6) || ($general_profile_compatibility_flag[6]) ||
                 ($general_profile_idc === 7) || ($general_profile_compatibility_flag[7]) ||
@@ -42,7 +42,8 @@ class IO_HEVC_ProfileTierLevel {
                 if (($general_profile_idc === 5) || ($general_profile_compatibility_flag[5]) ||
                     ($general_profile_idc === 9) || ($general_profile_compatibility_flag[9]) ||
                     ($general_profile_idc === 10) || ($general_profile_compatibility_flag[10])) {
-                    $this->general_max_14bit_constraint_flag = $bit->getUIBit();
+                    $this->general_max_14bit_constraint_flag =
+                                                             $bit->getUIBit();
                     $general_reserved_zero_33bits = $bit->getUIBits(33);
                     if ($general_reserved_zero_33bits !== 0) {
                         throw new Exception("ERROR: general_reserved_zero_33bits:$general_reserved_zero_33bits");
@@ -54,7 +55,7 @@ class IO_HEVC_ProfileTierLevel {
                     }
                 }
             } else {
-                $general_reserved_zero_43bits = $bit->getUIBits(34);
+                $general_reserved_zero_43bits = $bit->getUIBits(43);
                     if ($general_reserved_zero_43bits !== 0) {
                         throw new Exception("ERROR: general_reserved_zero_43bits:$general_reserved_zero_43bits");
                     }
@@ -75,20 +76,52 @@ class IO_HEVC_ProfileTierLevel {
                 }
             }
             $this->general_level_idc = $bit->getUIBits(8);
+            for () {
+            }
         }
     }
     function dump() {
         echo "    profile_tier_level:".PHP_EOL;
         $this->dump->printf($this, "        ( profilePresentFlag:%d maxnumSubLayersMinus1:%d )".PHP_EOL);
         if ($this->profilePresentFlag) {
+            $general_profile_idc = $this->general_profile_idc;
             $this->dump->printf($this, "        general_profile_space:%d general_tier_flag:%d general_profile_idc:%d".PHP_EOL);
             echo "        general_profile_compatibility_flag:";
-            foreach ($this->general_profile_compatibility_flag as $flag) {
+            $general_profile_compatibility_flag = $this->general_profile_compatibility_flag;
+            foreach ($general_profile_compatibility_flag as $flag) {
                 echo " ".$flag;
             }
             echo PHP_EOL;
             $this->dump->printf($this, "        general_progressive_source_flag:%d general_interlaced_source_flag:%d".PHP_EOL);
             $this->dump->printf($this, "        general_non_packed_constraint_flag:%d general_frame_only_constraint_flag:%d".PHP_EOL);
+            var_dump($general_profile_idc);
+            if (($general_profile_idc === 4) || ($general_profile_compatibility_flag[4]) ||
+                ($general_profile_idc === 5) || ($general_profile_compatibility_flag[5]) ||
+                ($general_profile_idc === 6) || ($general_profile_compatibility_flag[6]) ||
+                ($general_profile_idc === 7) || ($general_profile_compatibility_flag[7]) ||
+                ($general_profile_idc === 8) || ($general_profile_compatibility_flag[8]) ||
+                ($general_profile_idc === 9) || ($general_profile_compatibility_flag[9]) ||
+                ($general_profile_idc === 10) || ($general_profile_compatibility_flag[10])) {
+                $this->dump->printf($this, "        general_max_12bit_constraint_flag:%d general_max_10bit_constraint_flag:%d general_max_8bit_constraint_flag:%d".PHP_EOL);
+                $this->dump->printf($this, "        general_max_420chroma_constraint_flag:%d general_max_monochrome_constraint_flag:%d".PHP_EOL);
+                $this->dump->printf($this, "        general_intra_constraint_flag:%d general_one_picture_only_constraint_flag:%d general_lower_bit_rate_constraint_flag:%d".PHP_EOL);
+                if (($general_profile_idc === 5) || ($general_profile_compatibility_flag[5]) ||
+                    ($general_profile_idc === 9) || ($general_profile_compatibility_flag[9]) ||
+                    ($general_profile_idc === 10) || ($general_profile_compatibility_flag[10])) {
+                    $this->dump->printf($this, "        general_max_14bit_constraint_flag:%d".PHP_EOL);
+                }
+            }
+            if (((1 <= $general_profile_idc) && ($general_profile_idc <= 5)) ||
+                ($general_profile_idc === 9) ||
+                $general_profile_compatibility_flag[1] ||
+                $general_profile_compatibility_flag[2] ||
+                $general_profile_compatibility_flag[3] ||
+                $general_profile_compatibility_flag[4] ||
+                $general_profile_compatibility_flag[5] ||
+                $general_profile_compatibility_flag[9]) {
+                $this->dump->printf($this, "        general_inbld_flag:%d".PHP_EOL);
+            }
+            $this->dump->printf($this, "        general_level_idc:%d".PHP_EOL);
         }
     }
 }
