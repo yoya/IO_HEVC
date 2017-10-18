e0efb5f6d189426ea6ea249344621ebe5bf64fde
Yoshihiro Yamazaki <yoya@awm.jp>
2017-10-19 00:06:01 +0900

    HEVC parsing by IO_Bit & IO_HEVC_Bit combination.

--- IO/HEVC.php
+++ IO/HEVC.php
@@ -15,8 +15,10 @@ class IO_HEVC {
     var $_hevcdata = null;
 
     function parse($hevcdata, $opts = array()) {
-        $bit = new IO_HEVC_Bit();
+        $bit = new IO_Bit();
+        $bit_hevc = new IO_HEVC_Bit();
         $bit->input($hevcdata);
+        $bit_hevc->input($hevcdata);
         $this->_hevcdata = $hevcdata;
         $this->nalList = [];
         while ($bit->hasNextData(3)) {
@@ -28,10 +30,21 @@ class IO_HEVC {
             }
             $nal = new IO_HEVC_NAL();
             list($baseOffset, $dummy) = $bit->getOffset();
+            $bit_hevc->setOffset($baseOffset, 0);
             $nal->_offset = $baseOffset;
-            $nal->parse($bit);
+            $nal->parse($bit_hevc);
+            list($offset, $dummy) = $bit->getOffset();
+            $bit->setOffset($offset, 0);
             $bit->byteAlign();
             while ($bit->hasNextData(3) && $bit->getUIBits(24) !== 0x000001) {
+                if ($bit->hasNextData(4)) {
+                    if ($bit->getUI32BE() !== 0x00000001) {
+                        $bit->incrementOffset(-4, 0);
+                    } else {
+                        $bit->incrementOffset(-1, 0);
+                        break;
+                    }
+                }
                 $bit->incrementOffset(-2, 0);
             }
             $bit->incrementOffset(-3, 0);
